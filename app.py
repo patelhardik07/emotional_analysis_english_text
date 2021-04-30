0e8aa9ba1d210229ff4f4b91f14f61b1eddd497b
patelhardik07 <68460494+patelhardik07@users.noreply.github.com>
2021-04-30 09:09:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ def predict():
     padded = pad_sequences(seq, maxlen=max_seq_len)
     predictions = loaded_model.predict(padded)
     #output=class_names[np.argmax(predictions, axis=1)]
-    res['prediction']=predictions
+    pr=np.argmax(predictions)
+    res['prediction']=pr
     return jsonify(res)
 if __name__ == "__main__":
     app.run(port = 5000, debug=True)
