0ae9defa75279c703daac58b4ffdeb01a2e353b1
patelhardik07 <68460494+patelhardik07@users.noreply.github.com>
2021-04-30 12:59:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
 import re
 loaded_model = tf.keras.models.load_model('biLSTM_w2v.h5')
-import nltk
-nltk.download('punkt')
-def clean_text(data):
-  data = re.sub(r"(#[\d\w\.]+)", '', data)
-  data = re.sub(r"(@[\d\w\.]+)", '', data)
-  data = word_tokenize(data)
-  return data
+def clean_text(text):
+  text = text.lower()
+  new_text = re.sub('[^a-zA-z0-9\s]','',text)
+  new_text = re.sub('rt', '', new_text)
+  return new_text
 
 num_classes = 5
 
@@ -23,10 +21,11 @@ max_seq_len = 500
 
 class_names = ['joy', 'fear', 'anger', 'sadness', 'neutral']
 
-dat = pd.read_csv('data_train.csv')
-texts = [' '.join(clean_text(text)) for text in dat.Text]
+data_train = pd.read_csv('data_train.csv', encoding='utf-8')
+data_test = pd.read_csv('data_test.csv', encoding='utf-8')
+dat = data_train.append(data_test, ignore_index=True)
 tokenizer = Tokenizer()
-tokenizer.fit_on_texts(texts)
+tokenizer.fit_on_texts(dat['Text'])
 
 app = Flask(__name__)
 CORS(app)
